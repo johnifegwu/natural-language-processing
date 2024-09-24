5a62fadb4cfc53715308a396c97b8d4878cf79bf
John Ifegwu <57634092+johnifegwu@users.noreply.github.com>
2024-09-24 04:17:42 +0100

    Added .venv folder to gitignore

--- .venv/Lib/site-packages/linkedin_api/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-    linkedin-api
-"""
-
-from .linkedin import Linkedin
-
-__all__ = ["Linkedin"]
--- .venv/Lib/site-packages/linkedin_api/settings.py
+++
@@ -1,7 +0,0 @@
-import os
-from pathlib import Path
-
-HOME_DIR = str(Path.home())
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-LINKEDIN_API_USER_DIR = os.path.join(HOME_DIR, ".linkedin_api/")
-COOKIE_PATH = os.path.join(LINKEDIN_API_USER_DIR, "cookies/")
